405810a823a21ec77f9d47818965a78a55f7e674
Da_Person <hueychen27@outlook.com>
2022-08-10 06:42:28 -0700

    Added Arch Linux option

--- install_essentials.sh
+++ install_essentials.sh
@@ -8,10 +8,10 @@ Clear='\033[0m'
 
 echo "Type 1 for installing packages for Debian based distros. (Uses apt)"
 echo "Type 2 for installing packages for Red Hat based distros. (Uses dnf)"
+echo "Type 3 for installing packages for Arch Linux based distros. (Uses pacman)"
 
+printf ">> "
 read option
-
-# Check if number is not a number. If it is not a number, exit.
 
 re='^[0-9]+$'
 if ! [[ $option =~ $re ]]; then
@@ -27,6 +27,11 @@ elif [ $option -eq 2 ]; then
     sudo dnf install perl ruby
     echo -e "${Green}Done!${Clear}"
     exit 0
+elif [ $option -eq 3 ]; then
+    sudo pacman -S perl ruby
+    echo -e "${Green}Done!${Clear}"
+    exit 0
 else
     echo -e "${Red}Error! Enter a valid number. Exiting!${Clear}"
+    exit 1
 fi
