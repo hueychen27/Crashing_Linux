720f3a9d7a404eb881f26af156e3242f6cfd6125
Huey Chen <hueychen27@outlook.com>
2022-08-10 17:03:24 -0700

    Added php option

--- crash.sh
+++ crash.sh
@@ -10,6 +10,7 @@ echo "Use keyboard shortcut ctrl+c to exit."
 echo "Type 0 for bash fork."
 echo "Type 1 for perl fork."
 echo "Type 2 for ruby fork."
+echo "Type 3 for PHP fork."
 echo -e "${Red}Will crash your computer!${Clear}"
 
 printf ">> "
@@ -38,6 +39,13 @@ main() {
         sleep 4
         echo -e "${Red}Ruby Fork Bomb!1!!1!!1!!1!1!!1!1!1!1!11${Clear}"
         ruby fork_bomb.rb
+    elif [ $option == 3 ]; then
+        echo -e "${Green}In 5 seconds, this PHP fork bomb will run.${Clear}"
+        sleep 1
+        echo -e "${Green}Exit with ctrl+c.${Clear}"
+        sleep 4
+        echo -e "${Red}PHP Fork Bomb!!!!!!!!!!!111111111!!111${Clear}"
+        php -f fork_bomb.php
     else
         echo -e "${Red}Choose something valid. Exiting... Goodbye!${Clear}"
         exit 1
